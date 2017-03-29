fc38d513e9c1903869585cf59cb6566a8ebc7ba6
Luatt Nguyen <Luatt Nguyen>
2017-03-29 22:10:45 +0700

    Init

---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+$('.bgEffect').each(function(){
+    var $obj = $(this); $(window).scroll(function() {
+        var yPos = -($(window).scrollTop() / $obj.data('speed'));
+        var bgpos = '50% '+ yPos + 'px'; $obj.css('background-position', bgpos );
+    });
+});
+
+jQuery(document).ready(function($){
+    var timelineBlocks = $('.cd-timeline-block'),
+        offset = 0.8;
+
+    //hide timeline blocks which are outside the viewport
+    hideBlocks(timelineBlocks, offset);
+
+    //on scolling, show/animate timeline blocks when enter the viewport
+    $(window).on('scroll', function(){
+        (!window.requestAnimationFrame)
+            ? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+            : window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+    });
+
+    function hideBlocks(blocks, offset) {
+        blocks.each(function(){
+            ( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+        });
+    }
+
+    function showBlocks(blocks, offset) {
+        blocks.each(function(){
+            ( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+        });
+    }
+});
